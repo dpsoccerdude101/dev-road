36e6aae3cf8cbffe6f9b438eec7a60ea3e23734d
JamieVaughn <wxv6@case.edu>
2021-09-25 17:23:32 -0400

    Add ErrorBoundary component

---
+++ utils/ErrorBoundary.js
@@ -0,0 +1,36 @@
+// Wrap a component with <ErrorBoundary></ErrorBoundary>
+// to show an error UI if it or any child of it throws
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
+  
+  componentDidCatch(error, errorInfo) {
+    // Catch errors in any components below and re-render with error message
+    this.setState({
+      error: error,
+      errorInfo: errorInfo
+    })
+    // log error messages to an error reporting service here
+  }
+  
+  render() {
+    if (this.state.errorInfo) {
+      // Error path
+      return (
+        <div>
+          <h2>Something went wrong.</h2>
+          <details style={{ whiteSpace: 'pre-wrap' }}>
+            {this.state.error && this.state.error.toString()}
+            <br />
+            {this.state.errorInfo.componentStack}
+          </details>
+        </div>
+      );
+    }
+    // Normally, render children
+    return this.props.children;
+  }  
+}
