a85aed7990e070a64eb143548aafd5becaed0f25
Dennis Pavlyuk <37779084+dpsoccerdude101@users.noreply.github.com>
2021-10-30 00:08:38 -0400

    I have not tested this but this looks to be solely copy changes

--- content/syllabus.js
+++ content/syllabus.js
@@ -1,89 +1,91 @@
 export const rows = [
-    {level: ['i'], topic: 'Semantic HTML', hours: 9},
-    {level: ['i'], topic: 'Accessibility & UI/UX', hours: 3},
-    {level: ['i'], topic: 'Basic CSS', hours: 9},
-    {level: ['i'], topic: 'CSS Layout', hours: 15},
-    {level: ['i'], topic: 'Web Browsers & the Render Pipeline', hours: 3},
-    {level: ['i'], topic: 'Media Queries', hours: 3},
-    {level: ['i'], topic: 'Mobile Responsive Design', hours: 3},
-    {level: ['i'], topic: 'Images & SVG', hours: 6},
-    {level: ['i'], topic: 'GitHub & VSCode', hours: 6},
-    {level: ['i'], topic: 'Individual Portfolio Project', hours: 3},
-    {level: ['ii'], topic: 'Javascript Primitives', hours: 3},
-    {level: ['ii'], topic: 'Javascript Data Structures', hours: 6},
-    {level: ['ii'], topic: 'Procedural Javascript', hours: 3},
-    {level: ['ii'], topic: 'The DOM API', hours: 3},
-    {level: ['ii'], topic: 'Debugging', hours: 3},
-    {level: ['ii'], topic: 'Regular Expressions', hours: 3},
-    {level: ['ii'], topic: 'ES6 & Beyond', hours: 9},
-    {level: ['ii'], topic: 'Functional Programming Concepts', hours: 3},
-    {level: ['ii'], topic: 'The Event Loop', hours: 3},
-    {level: ['ii'], topic: 'Asynchronous Javascript', hours: 12},
-    {level: ['ii'], topic: 'Version Control with Github', hours: 3},
-    {level: ['ii'], topic: 'Javascript Design Patterns', hours: 6},
-    {level: ['ii'], topic: 'Javascript Application Architecture', hours: 3},
-    {level: ['iii'], topic: 'Node.js, Git & the Terminal', hours: 3},
-    {level: ['iii'], topic: 'SASS', hours: 3},
-    {level: ['iii'], topic: 'Intro to React & JS Frameworks', hours: 9},
-    {level: ['iii'], topic: 'Forms in React', hours: 3},
-    {level: ['iii'], topic: 'React Hooks', hours: 9},
-    {level: ['iii'], topic: 'React Build Tools', hours: 6},
-    {level: ['iii'], topic: 'React Router', hours: 3},
-    {level: ['iii'], topic: 'State Management (Redux)', hours: 6},
-    {level: ['iii'], topic: 'React/Redux Dev Tools', hours: 3},
-    {level: ['iii'], topic: 'Advanced React Patterns', hours: 3},
-    {level: ['iii'], topic: 'Project: Firebase CRUD Application', hours: 12},
-    {level: ['iii'], topic: 'Resume, Interviewing and Career Skills', hours: 6},
-  ].map((obj, ind) => ({...obj, key: ind}));
+  { level: ["i"], topic: "Semantic HTML", hours: 9 },
+  { level: ["i"], topic: "Accessibility & UI/UX", hours: 3 },
+  { level: ["i"], topic: "Basic CSS", hours: 9 },
+  { level: ["i"], topic: "CSS Layout", hours: 15 },
+  { level: ["i"], topic: "Web Browsers & the Render Pipeline", hours: 3 },
+  { level: ["i"], topic: "Media Queries", hours: 3 },
+  { level: ["i"], topic: "Mobile Responsive Design", hours: 3 },
+  { level: ["i"], topic: "Images & SVG", hours: 6 },
+  { level: ["i"], topic: "GitHub & VSCode", hours: 6 },
+  { level: ["i"], topic: "Individual Portfolio Project", hours: 3 },
+  { level: ["ii"], topic: "Javascript Primitives", hours: 3 },
+  { level: ["ii"], topic: "Javascript Data Structures", hours: 6 },
+  { level: ["ii"], topic: "Procedural Javascript", hours: 3 },
+  { level: ["ii"], topic: "The DOM API", hours: 3 },
+  { level: ["ii"], topic: "Debugging", hours: 3 },
+  { level: ["ii"], topic: "Regular Expressions", hours: 3 },
+  { level: ["ii"], topic: "ES6 & Beyond", hours: 9 },
+  { level: ["ii"], topic: "Functional Programming Concepts", hours: 3 },
+  { level: ["ii"], topic: "The Event Loop", hours: 3 },
+  { level: ["ii"], topic: "Asynchronous Javascript", hours: 12 },
+  { level: ["ii"], topic: "Version Control with Github", hours: 3 },
+  { level: ["ii"], topic: "Javascript Design Patterns", hours: 6 },
+  { level: ["ii"], topic: "Javascript Application Architecture", hours: 3 },
+  { level: ["iii"], topic: "Node.js, Git & the Terminal", hours: 3 },
+  { level: ["iii"], topic: "SASS", hours: 3 },
+  { level: ["iii"], topic: "Intro to React & JS Frameworks", hours: 9 },
+  { level: ["iii"], topic: "Forms in React", hours: 3 },
+  { level: ["iii"], topic: "React Hooks", hours: 9 },
+  { level: ["iii"], topic: "React Build Tools", hours: 6 },
+  { level: ["iii"], topic: "React Router", hours: 3 },
+  { level: ["iii"], topic: "State Management (Redux)", hours: 6 },
+  { level: ["iii"], topic: "React/Redux Dev Tools", hours: 3 },
+  { level: ["iii"], topic: "Advanced React Patterns", hours: 3 },
+  { level: ["iii"], topic: "Project: Firebase CRUD Application", hours: 12 },
+  { level: ["iii"], topic: "Resume, Interviewing and Career Skills", hours: 6 },
+].map((obj, ind) => ({ ...obj, key: ind }));
 
+export const details = [
+  {
+    title: "Time:",
+    content: "Mondays, Wednesdays & Thursdays, 6:30pm - 9:30pm",
+    icon: "notes.svg",
+  },
+  {
+    title: "Instructors:",
+    content:
+      "W. Jamie Vaughn wvaughniv@monroecc.edu, Dennis Pavlyuk dennispavlyuk@gmail.com, Keiran Pillman keiran.pillman@gmail.com",
+    icon: "notes.svg",
+  },
+];
 
-  export const details = [
-    {
-      "title": "Time:",
-      "content": "Tuesdays & Thursdays, 5:30pm - 8:30pm",
-      "icon": "notes.svg"
-    },
-    {
-      "title": "Instructor:",
-      "content": "W. Jamie Vaughn wvaughniv@monroecc.edu",
-      "icon": "notes.svg"
-    }
-  ]
-  
-  export const sections = [
-    {
-      "title": "Tool Tips!",
-      "content": "Overview of the most useful web development tools for every developer",
-      "icon": "checked_circle.svg"
-    },
-    {
-      "title": "Main Lecture Topic",
-      "content": "Review of concepts and web standards via slides and documentation",
-      "icon": "checked_circle.svg"
-    },
-    {
-      "title": "Survey of Computer Science",
-      "content": "Practical computer science topics and concepts",
-      "icon": "checked_circle.svg"
-    },
-    {
-      "title": "Code-Along",
-      "content": "Hands-on, guided coding exercises",
-      "icon": "checked_circle.svg"
-    },
-    {
-      "title": "Assessments & Challenges",
-      "content": "Quizes and challenges to test comprehension",
-      "icon": "checked_circle.svg"
-    },
-    {
-      "title": "Project Workshops",
-      "content": "Advisory sessions for working on assigned projects",
-      "icon": "checked_circle.svg"
-    },
-    {
-      "title": "Assigned Homework",
-      "content": "Homework exercises from Freecodecamp.org",
-      "icon": "checked_circle.svg"
-    }
-  ]
+export const sections = [
+  {
+    title: "Tool Tips!",
+    content:
+      "Overview of the most useful web development tools for every developer",
+    icon: "checked_circle.svg",
+  },
+  {
+    title: "Main Lecture Topic",
+    content:
+      "Review of concepts and web standards via slides and documentation",
+    icon: "checked_circle.svg",
+  },
+  {
+    title: "Survey of Computer Science",
+    content: "Practical computer science topics and concepts",
+    icon: "checked_circle.svg",
+  },
+  {
+    title: "Code-Along",
+    content: "Hands-on, guided coding exercises",
+    icon: "checked_circle.svg",
+  },
+  {
+    title: "Assessments & Challenges",
+    content: "Quizes and challenges to test comprehension",
+    icon: "checked_circle.svg",
+  },
+  {
+    title: "Project Workshops",
+    content: "Advisory sessions for working on assigned projects",
+    icon: "checked_circle.svg",
+  },
+  {
+    title: "Assigned Homework",
+    content: "Homework exercises from Freecodecamp.org",
+    icon: "checked_circle.svg",
+  },
+];
--- pages/syllabus/index.js
+++ pages/syllabus/index.js
@@ -1,45 +1,45 @@
-import { useState } from 'react'
-import { Table, Tag } from 'antd';
-import Layout from '../../components/Layout';
+import { useState } from "react";
+import { Table, Tag } from "antd";
+import Layout from "../../components/Layout";
 import styled from "styled-components";
 import ContentBlock from "../../components/ContentBlock";
 import MiddleBlock from "../../components/MiddleBlock";
-import { rows, details, sections } from "../../content/syllabus"
+import { rows, details, sections } from "../../content/syllabus";
 // import styles from './syllabus.module.css'
 
 // AntD <Table> config
 const columns = [
   {
-    title: 'Topic',
-    dataIndex: 'topic',
-    key: 'topic',
-    render: text => <a>{text}</a>,
+    title: "Topic",
+    dataIndex: "topic",
+    key: "topic",
+    render: (text) => <a>{text}</a>,
   },
   {
-    title: 'Level',
-    key: 'level',
-    dataIndex: 'level',
+    title: "Level",
+    key: "level",
+    dataIndex: "level",
     filters: [
       {
-        text: 'i',
-        value: 'i',
+        text: "i",
+        value: "i",
       },
       {
-        text: 'ii',
-        value: 'ii',
+        text: "ii",
+        value: "ii",
       },
       {
-        text: 'iii',
-        value: 'iii',
-      }
+        text: "iii",
+        value: "iii",
+      },
     ],
     onFilter: (value, record, index) => record.level.indexOf(value) === 0,
-    render: levels => (
+    render: (levels) => (
       <>
-        {levels.map(level => {
-          let color = level === 'i' ? 'geekblue' : 'green';
-          if (level.startsWith('iii')) {
-            color = 'volcano';
+        {levels.map((level) => {
+          let color = level === "i" ? "geekblue" : "green";
+          if (level.startsWith("iii")) {
+            color = "volcano";
           }
           return (
             <Tag color={color} key={level}>
@@ -51,45 +51,49 @@ const columns = [
     ),
   },
   {
-    title: 'Hours',
-    dataIndex: 'hours',
-    key: 'hours',
-  }
+    title: "Hours",
+    dataIndex: "hours",
+    key: "hours",
+  },
 ];
 
 const Padding = styled.div`
   max-width: 70vw;
   padding: 1rem;
   margin: 0 auto;
-`
+`;
 const Title = styled.h1`
   text-align: center;
-`
-const footer = num => (
+`;
+const footer = (num) => (
   <>
-    <strong>{'Total hours selected levels:'}</strong>
+    <strong>{"Total hours selected levels:"}</strong>
     <span></span>
     <span>{num}</span>
   </>
-)
+);
 
 const Syllabus = () => {
-  const sum = arr => arr.reduce((acc, cur) => acc += cur.hours, 0)
-  const [total, setTotal] = useState(sum(rows))
+  const sum = (arr) => arr.reduce((acc, cur) => (acc += cur.hours), 0);
+  const [total, setTotal] = useState(sum(rows));
 
   return (
     <>
       <Layout>
         <MiddleBlock
-            title={"MCC Front End Professional Certification"}
-            content={"Equip yourself with the modern skills and tools necessary to work as a professional Front End Developer or even launch your own online business venture! Certificates are awarded to students based on attendance and completion of homework & projects."}
-            icon={false}
-          />
-        <div className='syllabus-content'>
+          title={"MCC Front End Professional Certification"}
+          content={
+            "Equip yourself with the modern skills and tools necessary to work as a professional Front End Developer or even launch your online business venture! We award certificates to students based on attendance and completion of homework & projects."
+          }
+          icon={false}
+        />
+        <div className="syllabus-content">
           <ContentBlock
             type="left"
             title={"Class Details"}
-            content={"Classes are held via Open Source Jitsi Meet Teleconferencing"}
+            content={
+              "Classes are held via Discord Voice Channels Teleconferencing"
+            }
             section={details}
             icon={false}
           />
@@ -99,23 +103,33 @@ const Syllabus = () => {
             section={sections}
             dense={true}
             icon={false}
-            />
+          />
         </div>
-        <Title>Course Syllabus <a href='https://campusce.monroecc.edu/monroecc/course/course.aspx?C=828' target="_blank" rel="noreferrer"> 🔗</a></Title>
+        <Title>
+          Course Syllabus{" "}
+          <a
+            href="https://campusce.monroecc.edu/monroecc/course/course.aspx?C=848"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            🔗
+          </a>
+        </Title>
         <Padding>
-          <Table 
-            onChange={(page, sort, ev, filter) => setTotal(sum(filter.currentDataSource))}
-            size={'small'}
-            columns={columns} 
-            dataSource={rows} 
+          <Table
+            onChange={(page, sort, ev, filter) =>
+              setTotal(sum(filter.currentDataSource))
+            }
+            size={"small"}
+            columns={columns}
+            dataSource={rows}
             footer={() => footer(total)}
-              />
+          />
         </Padding>
       </Layout>
     </>
-  )
-}
+  );
+};
 
-export default Syllabus
-
-
+export default Syllabus;
